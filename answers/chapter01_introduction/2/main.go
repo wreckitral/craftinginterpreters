a299e8f2a1ce71c81457d67214ab21b4e1bd6247
wreckitral <defhanayasofhiea@gmail.com>
2024-07-09 09:55:47 +0700

    feat: introduction 2 answer

---
+++ answers/chapter01_introduction/2/main.go
@@ -0,0 +1,14 @@
+/*
+2. Get a “Hello, world!” program written and running in Java. Set up whatever makefiles or IDE projects you need to get it working.
+If you have a debugger, get comfortable with it and step through your program as it runs.
+
+but im just going to make it as Go instead.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello World!")
+}
